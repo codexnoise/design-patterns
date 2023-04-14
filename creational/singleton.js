68103bb44e5d229a4882bbb7e53efe4dd6c464dd
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2023-04-13 20:04:08 -0500

    add floder creationa

---
+++ creational/singleton.js
@@ -0,0 +1,7 @@
+/**
+ * How to implement Singleton?
+ *
+ * 1. Make the constructor private
+ * 2. Create a static method who calls the private
+ *  constructor and save the instance in a static variable
+ */
